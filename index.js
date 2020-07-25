0985654d7f2f45edeca9f4d7bffdb0f8506245d4
Andrew Echeverria <andrew.a.echeverria@gmail.com>
2020-07-25 12:44:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ var lineOrder = []
 for (let i = 0; i < katzDeliLine.length; i++){
   lineOrder.push(` ${i + 1}. ${katzDeliLine[i]}`)
     }
-return 'The line is currently:'+ lineOrder.join(", ")
+return 'The line is currently:'+ lineOrder.join(",")
 }// if (katzDeliLine.length === 0) {
   //return "The line is currently empty."
 //}
