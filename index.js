628b3fdaeb0259dc533909cb2305712d1481ca14
Andrew Echeverria <andrew.a.echeverria@gmail.com>
2020-07-25 12:43:07 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function takeANumber (line,name) {
+  line.push(name)
+  return `Welcome, ${name}. You are number ${line.length} in line.`
+}
+
+function nowServing (katzDeliLine) {
+ var i = 0
+ if (katzDeliLine.length === 0) {
+ return `There is nobody waiting to be served!`
+} else  {
+return `Currently serving ${katzDeliLine.shift()}.`
+ }
+}
+
+function currentLine (katzDeliLine) {
+var lineOrder = []
+for (let i = 0; i < katzDeliLine.length; i++){
+  lineOrder.push(` ${i + 1}. ${katzDeliLine[i]}`)
+    }
+return 'The line is currently:'+ lineOrder.join(", ")
+}// if (katzDeliLine.length === 0) {
+  //return "The line is currently empty."
+//}
